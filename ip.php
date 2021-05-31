0f361603fcb8b7fd2265b2688032095d28439648
fhtk02 <75006246+fhtk02@users.noreply.github.com>
2021-05-31 14:47:28 +0900

    Delete ip.php

--- ip.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$mysqli = new mysqli('localhost', 'root', 'root', 'sensor');
-	if($mysqli->connect_errno) {
-		echo '[MySQL 연결 오류]';
-	} else {
-		echo '[MySQL 연결 성공]';
-	}
-	mysqli_close($mysqli);
-?>
